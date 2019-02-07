6afb5d289a028e51608805e1d37203b09bea140a
Tacoalarmclock <45108867+Tacoalarmclock@users.noreply.github.com>
2019-02-06 18:26:13 -0800

    Level 5(IP)
    2/6/19

--- src/_02_File_Encrypt_Decrypt/FileDecryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileDecryptor.java
@@ -1,6 +1,32 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileDecryptor {
 	// Create a program that opens the file created by FileEncryptor and display
 	// the decrypted message to the user in a JOptionPane.
+	public static void main(String[] args) {
+
+		try {
+			FileReader fr = new FileReader("src/_02_File_Encrypt_Decrypt/test3.txt");
+			int c = fr.read();
+			String fileContents = "";
+			while (c != -1) {
+				fileContents += (char) ((c - 2) / 2);
+
+				c = fr.read();
+			}
+			fr.close();
+			System.out.println(fileContents);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,7 +1,32 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileEncryptor {
 	// Create a program that takes a message from the user.
 	// Use the methods in the String and Character classes to save
 	// an encrypted form of the message to a file
+
+	public static void main(String[] args) {
+		String yEncrypted = "";
+		String y = JOptionPane.showInputDialog("Give a message!");
+
+		for (int i = 0; i < y.length(); i++) {
+			yEncrypted += (char) (y.charAt(i) * 2 + 2);
+		}
+		System.out.println(yEncrypted);
+
+		try {
+			FileWriter fw = new FileWriter("src/_02_File_Encrypt_Decrypt/test3.txt");
+			// fw.write("\nThis is me writing a message");
+			fw.write("" + yEncrypted);
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
